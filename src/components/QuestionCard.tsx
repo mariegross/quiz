6ddbd0ea9662f6dd9f155739b06dbee37c92c3e2
mariegross <marie.gross@gmx.de>
2021-02-01 15:22:15 +0100

    ✨ Update App with all functions and finalize Card

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
+// Types
+import { AnswerObject } from '../App';
 
 type Props = {
     question: string;
     answers: string[];
-    callback: any;
-    userAnswer: any;
+    callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    userAnswer: AnswerObject | undefined;
     questionNr: number;
     totalQuestions: number;
 };
 
-const QuestionCard: React.FC<Props> = ({ question, answers, callback, userAnswer, questionNr, totalQuestions }) => (
+const QuestionCard: React.FC<Props> = ({
+    question,
+    answers,
+    callback,
+    userAnswer,
+    questionNr,
+    totalQuestions,
+}: Props) => (
     <div>
         <p className="number">
             Question: {questionNr} / {totalQuestions}
@@ -17,8 +26,8 @@ const QuestionCard: React.FC<Props> = ({ question, answers, callback, userAnswer
         <p dangerouslySetInnerHTML={{ __html: question }} />
         <div>
             {answers.map((answer) => (
-                <div>
-                    <button disabled={userAnswer} onClick={callback}>
+                <div key={answer}>
+                    <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
                         <span dangerouslySetInnerHTML={{ __html: answer }} />
                     </button>
                 </div>
